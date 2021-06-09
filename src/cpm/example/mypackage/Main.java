2c51b25160d1aaf4b97a1f72713dd8d14595734d
santiagowprr <jk.wrob@gmail.com>
2021-06-09 11:20:08 +0200

    Implemented whole program

--- src/cpm/example/mypackage/Main.java
+++ src/cpm/example/mypackage/Main.java
@@ -1,20 +1,54 @@
 package cpm.example.mypackage;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.OptionalInt;
+import java.util.*;
+
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] arrTemps = new int[3];
-
+        TempsManager tempsManager = new TempsManager();
+        ArrayList<Integer> arrTemps = new ArrayList<>();
+        arrTemps.add(10);
+        arrTemps.add(-7);
+        arrTemps.add(0);
+        arrTemps.add(100);
+        arrTemps.add(-7);
+        arrTemps.add(9);
+        arrTemps.add(-30);
+        arrTemps.add(-2);
+        arrTemps.add(-2);
+        ArrayList<Integer> result = tempsManager.getTempsAroundZero(arrTemps);
+        System.out.println(result); // [0]
 
     }
 }
 
 class TempsManager{
-    public int[] getTempsAroundZero(Integer[] inputTemps){
-        int tempAroundZero = Collections.max(Arrays.asList(inputTemps));
+    public ArrayList<Integer> getTempsAroundZero( ArrayList<Integer> inputTemps){
+        // creating new Arraylist to hold the final values
+        ArrayList<Integer> tempsAroundZero = new ArrayList<>();
+
+        int maxTemp = Collections.max(inputTemps);
+
+        for (Integer temperature : inputTemps){
+        // checking if there's a 0 in the array. If so, return arr with only one 0
+            if(temperature == 0){
+                tempsAroundZero.clear();
+                tempsAroundZero.add(0);
+                return tempsAroundZero;
+            }
+        // check which nr is closest to 0 and how often does it appear:
+            else if(Math.abs(temperature) < Math.abs(maxTemp)){
+                maxTemp = temperature;
+                tempsAroundZero.clear();
+                tempsAroundZero.add(temperature);
+
+                // if the checked temp is equal to the nearest to 0 - add it to the arr
+            } else if(temperature == maxTemp){
+                tempsAroundZero.add(temperature);
+            }
+        }
+        // return array of temps closest to 0
+        return tempsAroundZero;
     }
 }
